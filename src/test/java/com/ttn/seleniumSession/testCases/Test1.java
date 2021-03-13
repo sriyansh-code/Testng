39c285b88d93703d885fa7344f0dcf02189736fa
mridul <mridul.chawla@tothenew.com>
2021-03-13 19:38:28 +0530

    added first test

--- src/test/java/com/ttn/seleniumSession/testCases/Test1.java
+++ src/test/java/com/ttn/seleniumSession/testCases/Test1.java
@@ -1,4 +1,11 @@
 package com.ttn.seleniumSession.testCases;
 
+import org.testng.annotations.Test;
+
 public class Test1 {
+
+    @Test
+    public void test(){
+        System.out.println("First Test");
+    }
 }
